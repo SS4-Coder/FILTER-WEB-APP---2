b9e9c1800b3ccd0e3d6336117c44b83296a3425c
SS4-Coder <89593962+SS4-Coder@users.noreply.github.com>
2023-10-27 22:49:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+nose_x=0;
+nose_y=0;
+function preload()
+{
+
+}
+function setup(){
+canvas=createCanvas(300,240);
+canvas.center();
+video=createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+posenet=ml5.poseNet(video,model_loaded);
+posenet.on("pose",got_poses);
+}
+function draw(){
+image(video,0,0,300,240);
+}
+function take_snapshot(){
+
+}
+function model_loaded(){
+    console.log("model_loaded");
+    
+    }
+function got_poses(results){
+if(results.length>0){
+console.log(results);
+nose_x=results[0].pose.nose.x;
+nose_y=results[0].pose.nose.y;
+console.log("nose_x= "+nose_x);
+console.log("nose_y= "+nose_y);
+}
+}
